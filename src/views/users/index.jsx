bf1657c054605fd2aca3659917becf7f1d157a9f
ines <4cines@gmail.com>
2023-11-19 07:58:08 +0100

    add usersSlice

--- src/views/users/index.jsx
+++ src/views/users/index.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsersData } from "../../features/users/usersSlice";
 
 function Users() {
+  const usersData = useSelector((state) => state.users.data);
+  console.log(usersData);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsersData());
+  });
   return (
     <Box m="20px">
-      <Box
-        display={"flex"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-      >
+      <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
         <Header title="Usuarios" subtitle="Usuarios de la academia" />
       </Box>
     </Box>
